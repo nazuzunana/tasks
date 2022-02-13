c6a943d9da2e40c8ce8d9357d638800b436b910e
alice z <alisazakharenko@gmail.com>
2022-02-13 20:26:43 +0100

    initial commit
    Tlačítka - dělaji, co mají!
    Šipka - nebyla poskytnuta (?), vytvořila jsem vlastní SVG na Figmě, snad to je v pořádku.
    Logika je v > login.component.ts, styly > login.component.scss, markup > login.component.html
    P.S. Neměnila jsem nic mimo pokyny v issue, ale ideálně třída ‘my-login’ v login.component.scss  by měla být normálně ‘body’.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,12 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
+  title = 'Objednávkový systém'
+  first = ['Chci se', 'objednat na vyšetření']
+  second = ['Chci si', 'objednat recept']
+  first_active = false
+  second_active = true
+  cta = 'K objednání vyšetření'
+  
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  onClickFirst(event: Event) {
+    if (this.first_active) {
+      this.first_active = false
+      this.second_active = true
+      this.cta = 'K objednání vyšetření'
+    } 
+  }
+
+  onClickSecond(event: Event) {
+    if (this.second_active) {
+      this.first_active = true
+      this.second_active = false
+      this.cta = 'K objednání receptu'
+    }
   }
 
 }
